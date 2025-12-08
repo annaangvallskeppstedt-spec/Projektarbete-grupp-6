2c7b19e5df668d38e416188f73241319cbba2274
krisebe <ebertzkristin@gmail.com>
2025-12-08 15:12:26 +0100

    Add Navbar component and integrate UserContext provider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
-
+import React from "react"
+import Navbar from "../components/navbar"
 
 const Home = () => {
 
     return(<>
+
+    <Navbar/>
+
         <h1>Productivity Assistant App</h1>
         <h3>Login</h3>
     </>)
