7586dfbfcd9359de47201df8916625990113eb08
kristinebertz <ebertzkristin@gmail.com>
2025-12-18 11:03:22 +0100

    Add habit overview section to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,16 @@ import React from "react"
 import Navbar from "../components/navbar"
 import Login from "../components/login"
 import { useUser } from "../context/UserContext"
+import { HabitContext } from "../context/HabitContext"
+import { useContext } from "react"
 
 const Home = () => {
 
     const { isLoggedIn } = useUser()
+
+        const {
+            sortedHabits,
+        } = useContext(HabitContext)
 
     return(<>
 
@@ -18,7 +24,31 @@ const Home = () => {
         <Login />
         </>
         ) : (
-        <>    <h1>Översikt ?</h1> </>
+        <div className="container-overview">    <h1>Overview</h1> 
+
+        <div className="todo-overview">
+        <h3>My Todos</h3>
+        
+        </div>
+
+        <div className="event-overview">
+            <h3>My Events</h3>
+        </div>
+
+        <div className="habits-overview">
+            <h3>My Habits</h3>
+            <ul>
+                {sortedHabits && sortedHabits.length > 0 ? (
+                sortedHabits.map(habit => (
+                    <li key={habit.id}>{habit.title}</li>
+                ))
+            ) : ( 
+                <li>No habits yet</li>
+            )}
+            </ul>
+        </div>
+
+        </div>
         )
     }
     </>)
