92aee9de6eff692476068ce7613eaffd27b80245
kristinebertz <ebertzkristin@gmail.com>
2025-12-18 12:44:22 +0100

    Fix import case for Navbar component in multiple files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Navbar from "../components/navbar"
+import Navbar from "../components/Navbar"
 import Login from "../components/login"
 import { useUser } from "../context/UserContext"
 import { HabitContext } from "../context/HabitContext"
